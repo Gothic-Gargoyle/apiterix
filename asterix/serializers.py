23e8e04ca620ead54f2849356b6121002a3174e5
Gothic-Gargoyle <rijndershylbren@gmail.com>
2025-02-22 20:59:02 +0100

    linked items in models are now url clickable

--- asterix/serializers.py
+++ asterix/serializers.py
@@ -1,19 +1,19 @@
 from rest_framework import serializers
 from .models import Creator, Media, MediaTitle, Character, Alias, Location, Faction, Role
 
-class RoleSerializer(serializers.ModelSerializer):
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Role
         fields = '__all__'
 
-class CreatorSerializer(serializers.ModelSerializer):
+class CreatorSerializer(serializers.HyperlinkedModelSerializer):
     roles = RoleSerializer(many=True)  # Nested serializer
 
     class Meta:
         model = Creator
         fields = '__all__'
 
-class MediaSerializer(serializers.ModelSerializer):
+class MediaSerializer(serializers.HyperlinkedModelSerializer):
     writers = CreatorSerializer(many=True, read_only=True)
     illustrators = CreatorSerializer(many=True, read_only=True)
     directors = CreatorSerializer(many=True, read_only=True)
@@ -23,29 +23,29 @@ class MediaSerializer(serializers.ModelSerializer):
         model = Media
         fields = '__all__'
 
-class MediaTitleSerializer(serializers.ModelSerializer):
+class MediaTitleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MediaTitle
         fields = '__all__'
 
-class CharacterSerializer(serializers.ModelSerializer):
+class CharacterSerializer(serializers.HyperlinkedModelSerializer):
     media = MediaSerializer(many=True, read_only=True)
 
     class Meta:
         model = Character
         fields = '__all__'
 
-class AliasSerializer(serializers.ModelSerializer):
+class AliasSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Alias
         fields = '__all__'
 
-class LocationSerializer(serializers.ModelSerializer):
+class LocationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Location
         fields = '__all__'
 
-class FactionSerializer(serializers.ModelSerializer):
+class FactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Faction
         fields = '__all__'
